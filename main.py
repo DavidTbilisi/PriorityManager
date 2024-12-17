a025be96573b8014bd212d00f6a83624e5cdaf12
David <davidchincharashvili@gmail.com>
2024-12-18 02:35:27 +0400

    Add archive command to manage task archiving

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import click
 from commands.add import add
 from commands.edit import edit
 from commands.list_tasks import list_tasks
+from commands.archive import archive
 
 @click.group()
 def cli():
@@ -10,6 +11,7 @@ def cli():
 cli.add_command(add)
 cli.add_command(edit)
 cli.add_command(list_tasks)
+cli.add_command(archive)
 
 if __name__ == "__main__":
     cli()
